22d9eb27971b130f5557713ef71fcc29037cd7ff
Avindu Dinushan <hendawitharana.a.d@gmail.com>
2016-07-26 09:17:14 +0530

    New methods implemented

--- lesson_05/LinkedList.java
+++ lesson_05/LinkedList.java
@@ -1,4 +1,6 @@
 interface LinkedList{
+	boolean isEmpty();
+	int count();
 
 }
 
@@ -6,13 +8,16 @@ class EmptyNode implements LinkedList{
 	boolean isEmpty(){
 		return true;
 	}
+	int count(){
+		return 0;
+	}
 }
 
-class Node implements LinkedList{
-	int data;
+class Node<T> implements LinkedList{
+	T data;
 	Node next;
 
-	public Node(int data, Node next){
+	public Node(T data, Node next){
 		this.data = data;
 		this.next = next;
 	}
@@ -21,10 +26,20 @@ class Node implements LinkedList{
 		return l==null; 
 	}
 
+	int count(T item){
+		if(data==item){
+			return 1+count(next);
+		}else{
+			return count(next);
+		}
+	}
+
 	public static void main(String[] args) {
 		Node l0 = new EmptyNode();
-		Node l1 = new Node(1, new EmptyNode());
-		Node l2 = new Node(2,l1);
+		Node l1 = new Node<Integer>(1, new EmptyNode());
+		Node l2 = new Node<Integer>(1,l1);
+
+		System.out.println(l2.count(1));
 
 		System.out.println(isEmpty(l0) + " " + isEmpty(l2));
 	}
